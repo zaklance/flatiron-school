2d561b0356bc367493e5420ac23c5b17f79848ac
zaklance <wosewick@gmail.com>
2024-04-25 09:34:48 -0400

    add manipulation changes

--- phase-1/fundamentals/introduction/README.md
+++
@@ -1,4 +0,0 @@
-# An Introduction to HTML and JavaScript
-_Updated March 12, 2024_
-
-To get started, complete the core deliverables in [./beginning](./beginning) and open [./beginning/index.html](./beginning/index.html) in your browser. If you get stuck, take a peek at [./end](./end)!
--- phase-1/manipulation/beginning/index.js
+++ phase-1/manipulation/beginning/index.js
@@ -18,21 +18,63 @@ Challenges
 */
 
 // ~ APIs, CRUD, `document`
+// An application Programming Interface is a bridge between us and a data source.
+// API's let us Create, Read, Update, Delete data is such external sources.
+document // An API for communication between JS (this file) and HTML (index.html)
+ /*
+<body>
+    <p> Some text </p>
+    <div>
+        <a href="link.com">Link</a>
+    </div>
+</body>
+
+Dom
+            body
+              |
+        |----------|
+        p         div
+                   |
+                   p
+*/
+
 
 // ~ Read/Select a node
 // -> querySelector is a DOM method that lets us look up nodes by CSS **selector**.
 // -> querySelectorAll is a DOM method that returns an array-like list of nodes that match a CSS selector.
 // -> textContent is a property of text nodes (e.g. h1, p) that contain their text.
+console.log(document.querySelector('h1')); // read by tag name (singular)
+console.log(document.querySelector('#today')); // read by tag name
+console.log(document.querySelector('p.classy')); // read by class name
+console.log(document.querySelectorAll('p')); // read multiple elements at the same time
+console.log(document.querySelector('#today').textContent); // Read the text of the tag
 
+const p = document.querySelector('#today'); //  saving a node to a avariable
+console.log(p.textContent); // using a node variable
 
 // ~ Update/Modify a node's attributes
 // -> To modify an attribute, just use = after the attribute name in object dot notation
+document.querySelector('#forever').textContent = 'Forever ever'; // edits the text content
 
 // ~ Delete/Remove a node
 // -> To remove an existing element, we can look it up and call the node's method `remove()`.
+document.querySelector('#forever').remove(); // removes p 
 
 // ~ Create + Append a node
 // -> createElement(), append()
+const newH2 = document.createElement('h2');
+// parent.append(child); // append the element child as a child of the element parent
+newH2.textContent = 'H2 H2 H2';
+document.body.append(newH2) 
+
+
+/*
+        div
+         |
+        elememt
+         |
+        newElement
+*/
 
 // ~ Challenges
 // 1. Write a function named displayList that takes a name and an array,
@@ -40,7 +82,43 @@ Challenges
 //    the function should append to #dates something like:
 //        Books
 //        • The Shining
+// displayList('Books', ['The Shining', 'Eragon']); // after today and tomorrow, before foreveer
+
+// const newUl = document.createElement('ul');
+// const newLi = document.createElement('li');
+// document.querySelector('#dates').append(newUl);
+// document.querySelector('ul').textContent = 'Books';
+// document.querySelector('ul').append(newLi);
+// document.querySelector('li').textContent = 'The Shining';
+// document.querySelector('li').append(newLi);
+
+
+const books = ['The Shining', 'Eragon', 'Jurassic Park'];
+function displayList(name, array) {
+    const div = document.querySelector('#dates');
+    const heading = document.createElement('h2');
+    heading.textContent = name;
+    div.append(heading);
+    const list = document.createElement('ul');
+    array.forEach((item) => {
+        const li = document.createElement('li');
+        li.textContent = item;
+        list.append(li);
+    });
+    div.append(list);
+};
+displayList('books', books);
+
+// function displayListTwo(listName, listItems) {
+//     const parentElement =  document.querySelector('div#dates');
+
+//     const newList = document.createElement('ul');
+//     newList.textContent = listName;
+//     parentElement.append(newList);
+
+//     listItems.forEach(listItem => {
+//         let newItem 
+//     })
+
+
 // 2. Replace the <strong> element with a newly created one.
-
-
-
