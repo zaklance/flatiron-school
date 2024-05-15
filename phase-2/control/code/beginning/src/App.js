71800bc413cc226891afdfa07e0b906b02b279fe
zaklance <wosewick@gmail.com>
2024-05-15 14:14:25 -0400

    done

--- phase-2/control/code/beginning/src/App.js
+++ phase-2/control/code/beginning/src/App.js
@@ -14,14 +14,33 @@ Core Deliverables
 
 // To make `Card` a child of `App`, we need to import it (and render it) inside `App`.
 import Card from "./Card";
+import { useState } from "react";
 
 export default function App() {
+  // 1. Make a state that manages the current card
+  const [ card, setCard ] = useState("Ace of Spades")
+  // 2. Make arrays of suites and rank
+    const suits = ['Spades', 'Hearts', 'Clubs', 'Diamonds'];
+    const ranks = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'];
+  // 3. Make a function that randomly chooses from the arrays to change the card state
+  const randomizeCard = () => {
+    // a. Construct a new card
+    const randomRank = ranks[Math.floor(Math.random() * ranks.length)];
+    // a.ii. Get a random suite
+    const randomSuit = suits[Math.floor(Math.random() * ranks.length)];
+    // a.iii. Interpolate string
+    const randomCard = `${randomRank} of ${randomSuit}`;
+    // b. Call setCard with that new card
+    setCard(randomCard);
+  }
+  // 4. [Inside Card] Add a click event listener to the button that triggers the function
+  // 5. [Inside Card] Replace the static card text with the state value
   return (
     <div style={{ minHeight: "100vh",
                   display: "flex", flexDirection: "column",
                   alignItems: "center", justifyContent: "center" }}>
       <h1>Is This Your Card?</h1>
-      <Card />
+      <Card card={card} randomize={randomizeCard} />
       <footer>&copy; 2023 Sakib Rasul</footer>
     </div>
   );
--- phase-2/control/code/beginning/src/Card.js
+++ phase-2/control/code/beginning/src/Card.js
@@ -1,11 +1,11 @@
-export default function Card() {
+export default function Card({card, randomize}) {
     return (
         <h2 style={{ padding: "1rem",
                      display: "flex", flexDirection: "column", gap: "12px", alignItems: "center",
                      border: "solid black", borderRadius: "10px"
         }}>
-            Ace of Spades
-            <button>Nah.</button>
+            {card}
+            <button onClick={randomize}>Nah.</button>
         </h2>
     )
   }
