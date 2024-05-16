59d10d66de995854b0e9c626eed3d5c81be38b84
zaklance <wosewick@gmail.com>
2024-05-16 11:33:53 -0400

    done

--- phase-2/forms/code/beginning/src/Form.js
+++ phase-2/forms/code/beginning/src/Form.js
@@ -1,15 +1,34 @@
+import { useState } from 'react';
+
 export default function Form() {
+    // const [ name, setName ] = useState("");
+    // const [ movie, setMovie ] = useState("");
+    const [ memberData, setMemberData ] = useState({
+        name: "",
+        movie: ""
+    });
+    function handleChange(event){
+        setMemberData({
+            ...memberData, 
+            [event.target.name]: event.target.value
+        });
+    }
+    const character = memberData.movie.charAt(0)
+    let colorText = character === character.toUpperCase() ? "#000000" : "#f54242";
+
     return (
         <form>
             <label><strong>Name </strong>
-                <input />
+                <input name='name' value={memberData.name} onChange={handleChange} />
+                {/* <input value={memberData.name} onChange={event => setMemberData({ ...memberData, name: event.target.value })} /> */}
             </label>
             <br/><br/>
             <label><strong>Movie </strong>
-                <input />
+                <input style={{color: colorText}} name='movie' value={memberData.movie} onChange={handleChange} />
+                {/* <input value={memberData.movie} onChange={event => setMemberData({ ...memberData, movie: event.target.value })} /> */}
             </label>
             <br/><br/>
-            <em>Your lucky number is</em> <strong>2</strong>!
+            <em>Your lucky number is</em> <strong>{memberData.name.length * memberData.movie.length}</strong>!
             <br/><br/>
             <input type="submit" value="Sign Up"/>
             <h2>Members</h2>
